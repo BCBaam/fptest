a621e5deaf6b8fa06157fb1698c8a47750f9b5f9
Anton Seniuta <senita.anton@gmail.com>
2021-06-16 18:41:39 +0800

    feat: turn left and right gesture modified

--- src/TurnLeft.js
+++ src/TurnLeft.js
@@ -1,20 +1,12 @@
 import {Finger, FingerCurl, FingerDirection, GestureDescription} from "fingerpose";
 
 export const turnLeftGesture = new GestureDescription('turn_left');
-//ring
-turnLeftGesture.addCurl(Finger.Ring, FingerCurl.NoCurl, .9);
-turnLeftGesture.addDirection(Finger.Ring, FingerDirection.HorizontalLeft, 0.25);
-//pinky
-turnLeftGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, .9);
-turnLeftGesture.addDirection(Finger.Pinky, FingerDirection.HorizontalLeft, 0.25);
-//middle
-turnLeftGesture.addCurl(Finger.Middle, FingerCurl.NoCurl, .9);
-turnLeftGesture.addDirection(Finger.Middle, FingerDirection.HorizontalLeft, 0.25);
-//index
-turnLeftGesture.addCurl(Finger.Index, FingerCurl.NoCurl, .9);
-turnLeftGesture.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 0.25);
-//thumb
 
 
+for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+  turnLeftGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
+  turnLeftGesture.addDirection(finger, FingerDirection.HorizontalLeft, 0.7);
+  turnLeftGesture.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.7);
+}
 
 
--- src/TurnRight.js
+++ src/TurnRight.js
@@ -2,12 +2,8 @@ import {Finger, FingerCurl, FingerDirection, GestureDescription} from "fingerpos
 
 export const turnRightGesture = new GestureDescription('turn_right');
 
-turnRightGesture.addCurl(Finger.Ring, FingerCurl.NoCurl, .9)
-turnRightGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, .9)
-turnRightGesture.addCurl(Finger.Middle, FingerCurl.NoCurl, .9)
-turnRightGesture.addCurl(Finger.Index, FingerCurl.NoCurl, .9)
-
-turnRightGesture.addDirection(Finger.Ring, FingerDirection.HorizontalRight, 0.25)
-turnRightGesture.addDirection(Finger.Pinky, FingerDirection.HorizontalRight, 0.25)
-turnRightGesture.addDirection(Finger.Middle, FingerDirection.HorizontalRight, 0.25)
-turnRightGesture.addDirection(Finger.Index, FingerDirection.HorizontalRight, 0.25)
+for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+  turnRightGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
+  turnRightGesture.addDirection(finger, FingerDirection.HorizontalRight, 0.7);
+  turnRightGesture.addDirection(finger,FingerDirection.DiagonalUpRight, 0.7);
+}
